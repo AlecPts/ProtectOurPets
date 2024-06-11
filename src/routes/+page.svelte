bfd6b45c8d3505c5a1f5b6104aea2b8c3357dbe6
Alec Petit-Siejak <alecpetitsiejak@gmail.com>
2024-06-12 00:30:47 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,71 @@
 <script>
-
+	import Affiche from "$lib/images/poster-campaign.png";
+	import PhoneImage from "$lib/images/phone-image.png";
 </script>
 
 
-<div class="mx-5 my-14">
+<div class="max-w-4xl mx-auto my-14 text-xl/8 text-slate-600">
 	<h1 class="text-3xl font-semibold mb-5 text-center">Presentation of the POP App</h1>
-	<p>
+	<p class="">
 		ProtectAnimals is an innovative app designed to combat the mistreatment and abandonment of domestic animals.
 		This platform allows every citizen to quickly and easily report worrying situations involving animals in danger.
 		With an intuitive and user-friendly interface, users can provide detailed information about animals in distress,
 		such as photos, videos, and the exact geolocation of the incident.
 	</p>
+
+	<img src={Affiche} class="size-7/12 mx-auto my-10 drop-shadow-xl rounded" alt="poster campaign" />
 </div>
+
+<div class="my-20">
+	<div class="flex flex-row">
+		<div class="w-1/2">
+			<img src={PhoneImage} class="" alt="phone" />
+		</div>
+
+		<div class="w-1/2">
+			<p class="text-left">
+				The app offers an anonymous reporting feature, ensuring user safety while encouraging them to participate in this noble cause.
+				Reports are verified by a team of moderators and forwarded to the relevant authorities and partner animal protection organizations.
+				Additionally, ProtectAnimals includes a case tracking system, allowing users to monitor the progress of actions taken for each report.
+			</p>
+		</div>
+	</div>
+
+	<div class="flex flex-row">
+		<div class="w-1/2">
+			<p class="text-right">
+				As the founders of ProtectAnimals, we are passionate about animal welfare and believe that technology can play a key role in fighting abuse.
+				With years of experience in mobile app development and a deep empathy for animals, we created this platform to give a voice to those who cannot defend themselves.
+			</p>
+		</div>
+
+		<div class="w-1/2">
+			<p class="text-center">prout supreme</p>
+		</div>
+	</div>
+
+	<div class="flex flex-row">
+		<div class="w-1/2">
+			<p class="text-center">prout eternel</p>
+		</div>
+
+		<div class="w-1/2">
+			<p class="text-left">
+				ProtectAnimals aims not only to save animal lives but also to raise public awareness about the importance of respecting and protecting animals.
+				Together, through this app, we can make a real difference and build a more just and compassionate world for our furry friends. Join us in this mission and be part of the change with ProtectAnimals.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div>
+	<p class="text-center">
+		As you may know, many animals are living in very poor conditions, such as chickens being force-fed by farmers, cows being pressured to produce excessive milk, or animals being abandoned in the wild.
+		Our project is dedicated to addressing this issue. [Link to our project page]
+	</p>
+</div>
+
+
+<style>
+
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,19 +1,21 @@
 <script>
-	import Logo from '.././lib/images/logo-variants/pop_lightgreen.png';
+	import Logo from '$lib/logo-variants/pop_lightgreen.png';
 	import { page } from '$app/stores';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 	$: activeUrl = $page.url.pathname;
 </script>
   
-<Navbar class="drop-shadow-lg">
+<Navbar class="fixed z-10 top-0 drop-shadow-lg bg-primary-50">
 	<NavBrand href="/">
 		<img src={Logo} class="mx-5 my-3 h-12" alt="POP Logo" />
 	</NavBrand>
+
 	<NavHamburger />
+
 	<NavUl {activeUrl}>
-		<NavLi href="/">About</NavLi>
-		<NavLi href="/quizz">Quizz and Poll</NavLi>
-		<NavLi href="/report">Report</NavLi>
-		<NavLi href="/donation">Donation</NavLi>
+		<NavLi href="/" class="text-lg">About</NavLi>
+		<NavLi href="/quizz" class="text-lg">Quizz and Poll</NavLi>
+		<NavLi href="/report" class="text-lg">Report</NavLi>
+		<NavLi href="/donation" class="text-lg">Donation</NavLi>
 	</NavUl>
 </Navbar>
